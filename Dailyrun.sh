b3260a661b18250bc61361cfae438d796e3f021f
nelsonchung <chihchun.chung@gmail.com>
2016-06-02 09:33:32 +0800

    Update by 16:30

--- Dailyrun.sh
+++ Dailyrun.sh
@@ -1,5 +1,20 @@
-python crawl.py
-python TWT38U.py
-python TWT43U.py
-python TWT44U.py
-python Parser.py
+HOUR_RUN_STOCK=16
+MINUTE_RUN_STOCK=30
+
+
+while [ 1 ]
+do
+    HOUR=`date +"%H"`
+    MINUTE=`date +"%M"`
+    DAY_OF_WEEK=`date +"%u"`
+    if [ "$DAY_OF_WEEK" == "1" ] || [ "$DAY_OF_WEEK" == "2" ] || [ "$DAY_OF_WEEK" == "3" ] || [ "$DAY_OF_WEEK" == "4" ] || [ "$DAY_OF_WEEK" == "5" ] ; then
+        if [ "$HOUR" == "$HOUR_RUN_ETF" ] && [ "$MINUTE" == "$MINUTE_RUN_ETF" ]; then
+        python crawl.py
+        python TWT38U.py
+        python TWT43U.py
+        python TWT44U.py
+        python Parser.py
+        fi
+    fi
+    sleep 60
+done
