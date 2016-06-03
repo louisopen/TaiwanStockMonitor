81bc354b9696cfff92209abad4a887e411e09e89
nelsonchung <chihchun.chung@gmail.com>
2016-06-03 09:55:41 +0800

    Monitor 2498 and 2454

--- Dailyrun.sh
+++ Dailyrun.sh
@@ -8,12 +8,14 @@ do
     MINUTE=`date +"%M"`
     DAY_OF_WEEK=`date +"%u"`
     if [ "$DAY_OF_WEEK" == "1" ] || [ "$DAY_OF_WEEK" == "2" ] || [ "$DAY_OF_WEEK" == "3" ] || [ "$DAY_OF_WEEK" == "4" ] || [ "$DAY_OF_WEEK" == "5" ] ; then
+    echo "Run TaiwanStockMonitor at 星期${DAY_OF_WEEK}, ${HOUR}點${MINUTE}分"
         if [ "$HOUR" == "$HOUR_RUN_STOCK" ] && [ "$MINUTE" == "$MINUTE_RUN_STOCK" ]; then
         python crawl.py
         python TWT38U.py
         python TWT43U.py
         python TWT44U.py
-        python Parser.py
+        python Parser.py 2498
+        python Parser.py 2454
         fi
     fi
     sleep 60
